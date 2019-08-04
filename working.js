298a71790fd8ff8dc8435b0640efe008abc7e713
MikaeI <mikael@persille.no>
2019-08-04 12:34:56 +0200

    Misc

--- working.js
+++ working.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 let state = JSON.parse(fs.readFileSync('./node_modules/replenish/state.json'));
-state.updated = Date.now();
-state.working = false;
+state.working = true;
 fs.writeFile("./node_modules/replenish/state.json", JSON.stringify(state), 'utf8', function (err) {
     if (err) {
         return console.log(err);
